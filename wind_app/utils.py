1d76009180ab14c6c85a27bed83c0a9424de6dbc
Piotr Grędowski <pgred@orsted.com>
2025-07-19 20:57:17 +0200

    feat(utils): add common function for logging

---
+++ wind_app/utils.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+
+def log(data: Any) -> None:
+    """
+    Simple logging function to print data to console.
+    This can be replaced with a more sophisticated logging system if needed.
+
+    Args:
+        data: Data to log, can be any type.
+    """
+    print(data)
